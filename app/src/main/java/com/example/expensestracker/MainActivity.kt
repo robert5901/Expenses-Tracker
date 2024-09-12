0cef5bf51e7c6e31a967433ddb26cdf79edb6be9
robert5901 <Shadow0079005>
2024-09-12 20:44:02 +0300

    Created general screen ui

--- app/src/main/java/com/example/expensestracker/MainActivity.kt
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.expensestracker
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.expensestracker.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
-    private val viewBinding by viewBinding(ActivityMainBinding::bind)
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val generalFragment = GeneralFragment()
+
+        supportFragmentManager.beginTransaction().add(
+            R.id.main_container,
+            generalFragment,
+            "123"
+        ).commit()
     }
 }
 
